b8cf5af705fb7b7223c0f746ebe625b868601b64
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-01-08 00:21:40 +0000

    Start App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
+import { fetchPictures } from './lib/api';
+
+import DogCardInfo from './components/DogCardInfo';
+import BreedList from './components/BreedList';
+
 import './App.css';
 
 function App() {
+  const [pictures, setPictures] = useState([]);
+  const [selectedBreedId, setSelectBreedId] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const loadPictures = async () => {
+      if (selectedBreedId !== '') {
+        setIsLoading(loading => !loading);
+        const fetchedPictures = await fetchPictures(selectedBreedId, 20);
+        setPictures(fetchPictures);
+        setIsLoading(loading => !loading);
+      }
+    };
+    loadPictures();
+  }, [selectedBreedId]);
   return (
-    <h1>Welcome to furry friends gallery 2</h1>
+   <div className = 'container'>
+    <header className = 'section has-text-centered'>
+      <h1 className = 'title is-size-3 has-text-primary'>
+        Search dog pictures
+      </h1>
+      <p>Filter by breed for more choices</p>
+    </header>
+    <hr />
+   </div>
   );
 }
 
